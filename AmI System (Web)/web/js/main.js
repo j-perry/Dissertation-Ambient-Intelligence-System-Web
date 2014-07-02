470ba33b462a407308cc9e925984520de2965b61
Jonathan Perry <j.m.perry@hotmail.co.uk>
2014-07-02 22:30:29 +0100

    CSS/JS commenting

--- AmI System (Web)/web/js/main.js
+++ AmI System (Web)/web/js/main.js
@@ -1,4 +1,14 @@
+
+
 /* 
  * The main JavaScript file for our Ambient Intelligence web application.
  * All code has been written with reusability in mind.
  */
+jQuery.noConflict();
+
+// when the view has loaded
+$(document).ready(function() {
+    
+    
+    
+});
