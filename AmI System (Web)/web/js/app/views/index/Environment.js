f35ff048eb9675a58f1e632640a513baa25d44eb
Jonathan Perry <j.m.perry@hotmail.co.uk>
2014-07-14 22:35:41 +0100

    Added paths (x3)

--- AmI System (Web)/web/js/app/views/index/Environment.js
+++ AmI System (Web)/web/js/app/views/index/Environment.js
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-var path = "";
+var path = "../jp373/js/app/views/index/Environment";
 
 define(path, function() {
     function Environment() {
--- AmI System (Web)/web/js/app/views/index/SystemHistory.js
+++ AmI System (Web)/web/js/app/views/index/SystemHistory.js
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-var path = "";
+var path = "../jp373/js/app/views/index/SystemHistory";
 
 define(path, function() {
     function SystemHistory() {
