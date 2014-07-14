c6c987cc10c4b37c0aa056b6a05b3ec4cc15a075
Jonathan Perry <j.m.perry@hotmail.co.uk>
2014-07-14 22:33:36 +0100

    Added x3 new sub-classes to Index.js

---
+++ AmI System (Web)/web/js/app/views/index/Welcome.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var path = "";
+
+define(path, function() {
+    function Welcome() {
+        
+        
+        
+    };
+});
