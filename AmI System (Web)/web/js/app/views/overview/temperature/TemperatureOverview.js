23c206b5498470ffef11a275c8d5395a4d7a556c
Jonathan Perry <j.m.perry@hotmail.co.uk>
2014-08-14 21:22:31 +0100

    Added code

--- AmI System (Web)/web/js/app/views/overview/temperature/TemperatureOverview.js
+++ AmI System (Web)/web/js/app/views/overview/temperature/TemperatureOverview.js
@@ -6,8 +6,18 @@ var path = "../jp373/js/app/views/index/Environment/TemperatureOverview.js";
 define(path, function(temperatureOverview) {
     function TemperatureOverview() {
         
+        /**
+         * 
+         */
         this.display = function() {
+            var path = "http://tomcat.inf.susx.ac.uk:8080/jp373/js/temperature.json";
             
+            // get JSON values
+            $.getJSON(path, function(data) {
+               
+                
+                
+            });
         };
         
     }
