c825de018345c549b91c3b29110f6b1021041804
Jonathan Perry <j.m.perry@hotmail.co.uk>
2014-08-14 21:40:57 +0100

    Added field properties

--- AmI System (Web)/src/java/ami/web/core/servlets/modules/SystemOverview.java
+++ AmI System (Web)/src/java/ami/web/core/servlets/modules/SystemOverview.java
@@ -3,11 +3,24 @@
 
 package ami.web.core.servlets.modules;
 
+import ami.web.core.db.InitialTable;
+import ami.web.core.db.SystemInfoTable;
+import ami.web.core.models.client.ClientInfo;
+
+import java.io.FileWriter;
+import org.json.simple.JSONObject;
+
 /**
  *
  * @author Jonathan Perry
  */
 public class SystemOverview {
+    
+    private ClientInfo clientInfo;
+    private InitialTable initialTable;
+    private SystemInfoTable systemInfoTable;
+    private JSONObject overview;
+    private FileWriter fWriter;
     
     public SystemOverview() {
         
