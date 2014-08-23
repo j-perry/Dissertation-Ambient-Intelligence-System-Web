0be2881f3a09565ea14d08c5d8c150001737755f
Jonathan Perry <j.m.perry@hotmail.co.uk>
2014-08-23 09:13:03 +0100

    Implemented chart functionality for displayMonday()

--- AmI System (Web)/web/js/app/views/temperature/Weekdays.js
+++ AmI System (Web)/web/js/app/views/temperature/Weekdays.js
@@ -115,6 +115,14 @@ define(path, function(weekdays) {
                         }
                     ]
                 };
+                
+                // render chart
+                var ctx = document.getElementById("temperatureMonday").getContext("2d");
+                var myLineChart = new Chart(ctx).Line(data, {
+                    bezierCurve: false,
+                    scaleShowGridLines: false,
+                    scaleGridLineColor: "rgba(0,0,0, 1)"
+                });
 
             });
 
